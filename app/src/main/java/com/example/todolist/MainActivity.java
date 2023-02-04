94cd228eee6be66ea32b5a29c9442c7afa34bea9
coffe4wolf <coffe4wolf@gmail.com>
2023-02-04 18:53:34 +0300

    Initial commit.

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewNotes;
+    private FloatingActionButton buttonAddNote;
+    private NotesAdapter notesAdapter;
+
+    private Database database = Database.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        notesAdapter = new NotesAdapter();
+
+        recyclerViewNotes.setAdapter(notesAdapter);
+
+
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = AddNoteActivity.newIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+    protected void onResume() {
+        super.onResume();
+        showNotes();
+    }
+
+
+    private void initViews(){
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
+    }
+
+    private void showNotes() {
+        notesAdapter.setNotes(database.getNotes());
+    }
+}
